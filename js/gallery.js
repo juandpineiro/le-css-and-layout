84bfbd7656ea2068343bae10fd2acd7b043a311d
Juan Piñeiro <juandpineiro@gmail.com>
2019-12-04 19:45:36 -0400

    Add gallery activating JavaScript

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,15 @@
+// Activates the image gallery
+// The main task is to attach an event listener to each image in the gallery
+// and respond appropriately on click
 function activateGallery() {
-  alert("Hello from the gallery file!");
+  let thumbnails = document.querySelector("#gallery-thumbs").
+                            querySelectorAll("img");
+  let mainImage = document.querySelector("#gallery-photo img");
+
+  thumbnails.forEach(function(thumbnail) {
+    thumbnail.addEventListener("click", function() {
+      let newImageSrc = thumbnail.dataset.largeVersion;
+      mainImage.setAttribute("src", newImageSrc);
+    })
+  });
 }
