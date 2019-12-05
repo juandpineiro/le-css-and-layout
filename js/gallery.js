e8db210275fd4fe1188c6987f07ad396ccdbdb68
Juan Piñeiro <juandpineiro@gmail.com>
2019-12-04 20:32:09 -0400

    Finish the JavaScript gallery

--- js/gallery.js
+++ js/gallery.js
@@ -2,14 +2,25 @@
 // The main task is to attach an event listener to each image in the gallery
 // and respond appropriately on click
 function activateGallery() {
-  let thumbnails = document.querySelector("#gallery-thumbs").
-                            querySelectorAll("img");
+  let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
   let mainImage = document.querySelector("#gallery-photo img");
 
   thumbnails.forEach(function(thumbnail) {
     thumbnail.addEventListener("click", function() {
+      // Set clicked image as display image
       let newImageSrc = thumbnail.dataset.largeVersion;
       mainImage.setAttribute("src", newImageSrc);
-    })
+      mainImage.setAttribute("alt", thumbnail.dataset.title);
+
+      // Change which image is current
+      document.querySelector(".current").classList.remove("current");
+      thumbnail.parentNode.classList.add("current");
+
+      // Update the image info
+      let title = document.querySelector("#gallery-info .title");
+      let description = document.querySelector("#gallery-info .description");
+      title.innerHTML = thumbnail.dataset.title;
+      description.innerHTML = thumbnail.dataset.description;
+    });
   });
 }
